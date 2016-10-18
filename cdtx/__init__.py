5b9ed983015fc1de92f49e77b8df44aa39692454
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2016-10-18 13:32:14 +0200

    Namespace package __init__.py file better do not start with a comment

--- cdtx/__init__.py
+++ cdtx/__init__.py
@@ -1,5 +1,6 @@
+__import__('pkg_resources').declare_namespace(__name__)
+
 # This is an implicit namespace package (https://www.python.org/dev/peps/pep-0420/)
 # It's built with the help of the setuptools's functionality : (http://setuptools.readthedocs.io/en/latest/setuptools.html#namespace-packages)
-__import__('pkg_resources').declare_namespace(__name__)
 
 # No other code shall be written after.
